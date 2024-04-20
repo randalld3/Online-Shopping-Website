951c01c6abb6694cb6f160ade7ccf9e712625dd2
Aidan Johnson <johnsoac671@gmail.com>
2024-04-20 14:51:27 -0700

    created a model for storing previous orders

---
+++ models/Order.js
@@ -0,0 +1,47 @@
+const sequelize = require("../db")
+const { Model, DataTypes } = require("sequelize")
+
+class Order extends Model {
+
+    static async findProduct(id){
+        try{
+            const order = await Order.findByPk(id)
+
+            return order ? order : null
+        } catch (error){
+            console.log(error)
+            return null
+        }
+    }
+}
+
+Order.init({
+
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+  },
+
+  user: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  items: {
+    type: DataTypes.JSON,
+    allowNull: false
+  },
+
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+
+  sequelize, 
+  modelName: 'Order' 
+});
+
+module.exports = Order
